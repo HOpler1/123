b6339df8b5e468cdd2393a99ac4ef1147870f510
HOpler1 <97186359+HOpler1@users.noreply.github.com>
2022-04-12 11:48:07 +0900

    Add files via upload

--- calc/calc/ViewController.swift
+++ calc/calc/ViewController.swift
@@ -8,11 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var buttonAC: UIButton!
+    @IBOutlet weak var buttonZero: UIButton!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var buttonChangeResultLabel: UIStepper!
+    
+    @IBOutlet weak var buttons: UIStackView!
+    
+    var numberOne = ""
+    var numberTwo = ""
+    var operand = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        buttonAC.layer.cornerRadius = 36
+    
     }
+    
+    
+    @IBAction func inputNumber(_ sender: UIButton) {
+        if operand.isEmpty {
+            (sender.titleLabel?.text)!
+            resultLabel.text = numberOne
+        }
+        else
+    }
+  
 
 
 }
